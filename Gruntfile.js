373ecdc38dac1d0c269c0462ee3df62557b4439f
Demur Rumed <gunkmute@gmail.com>
2013-12-10 23:36:03 +0000

    Forgot to include Gruntfile.js

---
+++ Gruntfile.js
@@ -0,0 +1,16 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON("package.json"),
+		concat: {
+			options: {
+				stripBanners: {all: true},
+			},
+			dist: {
+				src: ["MersenneTwister.js", "etg.js", "pixi.js"],
+				dest: "js.js",
+			}
+		}
+	});
+	grunt.loadNpmTasks("grunt-contrib-concat");
+	grunt.registerTask("default", ["concat"]);
+}
