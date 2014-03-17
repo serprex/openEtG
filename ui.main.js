99431f15556c053fb8d22ac358c69b09805784d7
Seth Kips <selite1134@gmail.com>
2014-03-17 14:06:02 -0700

    Remove Debug Code from Previous Commit
    Removed shop debug code that allowed for obtaining free booster packs

--- ui.main.js
+++ ui.main.js
@@ -1125,7 +1125,7 @@ function startStore() {
 	var bbuy = makeButton(750, 150, 75, 18, "Buy");
 	bbuy.click = function() {
 		if (isEmpty(newCards)) {
-			if (true) { //if (user.gold >= 30) {
+			if (user.gold >= 30) {
 				var allowedElements = []
 
 				if (packtype == 1) allowedElements = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
@@ -1138,7 +1138,7 @@ function startStore() {
 					return;
 				}
 				
-				//user.gold -= 30;
+				user.gold -= 30;
 				userEmit("subgold", { g: 30 });
 				
 				for (var i = 0; i < 10; i++) {
