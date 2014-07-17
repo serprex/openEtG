1143f21148f336a0e8503545430f22390d717ac1
Demur Rumed <gunkmute@gmail.com>
2014-07-17 14:26:31 +0000

    I'll never learn __dirname

--- etg.server.js
+++ etg.server.js
@@ -4,7 +4,7 @@ exports.loadcards = function(cb){
 	var Cards = {}, CardCodes = {}, Targeting = {};
 	var names = ["pillar", "weapon", "shield", "permanent", "spell", "creature"];
 	for(var i=0; i<names.length; i++){
-		var csv = fs.readFileSync(names[i] + ".csv").toString().split("\n");
+		var csv = fs.readFileSync(__dirname + "/" + names[i] + ".csv").toString().split("\n");
 		var keys = csv[0].split(",");
 		for(var j=1; j<csv.length; j++){
 			var carddata = csv[j].split(",");
@@ -25,7 +25,7 @@ exports.loadcards = function(cb){
 			Cards[nospacename in Cards?nospacename+"Up":nospacename] = CardCodes[cardcode] = new etg.Card(i, cardinfo);
 		}
 	}
-	var csv = fs.readFileSync("active.csv").toString().split("\n");
+	var csv = fs.readFileSync(__dirname + "/" + "active.csv").toString().split("\n");
 	for (var i=0; i<csv.length; i++){
 		var keypair = csv[i].split(",");
 		Targeting[keypair[0]] = etg.getTargetFilter(keypair[1]);
