abd6f12cb78319b7e2bd4bdf597d107b6376e1bd
Demur Rumed <gunkmute@gmail.com>
2014-08-06 01:02:42 +0000

    Remove double deck from mkPremade

--- ui.main.js
+++ ui.main.js
@@ -545,7 +545,6 @@ function mkPremade(name, daily) {
 		var foedata = daily ? aiDecks[name][user[name == "mage" ? "dailymage" : "dailydg"]] : aiDecks.giveRandom(name);
 		var foename = name[0].toUpperCase() + name.slice(1) + "\n" + foedata[0];
 		var deck = (!user && aideck.value.split(" ")) || etgutil.decodedeck(foedata[1]);
-		deck = deck.concat(deck);
 		var gameData = { first: Math.random() < .5, deck: deck, urdeck: urdeck, seed: Math.random() * etgutil.MAX_INT, foename: foename };
 		if (name == "mage"){
 			gameData.hp = 125;
