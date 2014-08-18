48a772998c3c412fea7596bf3e8336586c09306e
Demur Rumed <gunkmute@gmail.com>
2014-08-18 14:35:40 +0000

    Fix shiny redirect, remove space between hp and mark, cock code

--- server.js
+++ server.js
@@ -129,15 +129,13 @@ function codeSmith(req, res, next){
 function cardRedirect(req, res, next){
 	if (req.url.match(/^\/Cards\/...\.png$/)){
 		var code = req.url.substr(7, 3);
-		if (code < "4s8" || code > "6qn"){
-			fs.exists(__dirname + req.url, function(exists){
-				if (!exists){
-					res.writeHead("302", {Location: "http://" + req.headers.host + "/Cards/" + etgutil.asShiny(etgutil.asUpped(code, false), false) + ".png"});
-					res.end();
-				}else next();
-			});
-			return;
-		}
+		fs.exists(__dirname + req.url, function(exists){
+			if (!exists){
+				res.writeHead("302", {Location: "http://" + req.headers.host + "/Cards/" + etgutil[code >= "g00"?"asShiny":"asUpped"](code, false) + ".png"});
+				res.end();
+			}else next();
+		});
+		return;
 	}
 	next();
 }
