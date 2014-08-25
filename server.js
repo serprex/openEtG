a14c1d417ad14df4e26fbad7a477dc0c5bae6ead
Demur Rumed <gunkmute@gmail.com>
2014-08-25 18:59:53 +0000

    Lighten upped cards in /deck/

--- server.js
+++ server.js
@@ -146,7 +146,9 @@ function deckRedirect(req, res, next){
 		var deck = req.url.substr(6);
 		var Canvas = require("canvas"), Image = Canvas.Image;
 		var can = new Canvas(616, 160), ctx = can.getContext("2d");
-		var elecols = ["#a99683", "#aa5999", "#777777", "#996633", "#5f4930", "#50a005", "#cc6611", "#205080", "#a9a9a9", "#337ddd", "#ccaa22", "#333333", "#77bbdd"];
+		var elecols = [
+			"#a99683", "#aa5999", "#777777", "#996633", "#5f4930", "#50a005", "#cc6611", "#205080", "#a9a9a9", "#337ddd", "#ccaa22", "#333333", "#77bbdd",
+			"#d4cac1", "#d4accc", "#bbbbbb", "#ccb299", "#afa497", "#a7cf82", "#e5b288", "#8fa7bf", "#d4d4d4", "#99beee", "#e5d490", "#999999", "#bbddee"];
 		ctx.font = "11px Dosis";
 		ctx.textBaseline = "top";
 		etgutil.iterdeck(deck, function(code, i){
@@ -160,7 +162,7 @@ function deckRedirect(req, res, next){
 			if (!(code in Cards.Codes))return;
 			var card = Cards.Codes[code];
 			var x = 16+Math.floor(i/10)*100, y = (i%10)*16;
-			ctx.fillStyle = elecols[card.element];
+			ctx.fillStyle = elecols[card.element+(card.upped?13:0)];
 			ctx.fillRect(x, y, 100, 16);
 			ctx.fillStyle = "#000000";
 			ctx.fillText(card.name, x, y);
