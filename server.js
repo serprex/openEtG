046960c635566da9eae45f87d2a91a2ca41d0f3c
Demur Rumed <gunkmute@gmail.com>
2014-09-02 22:25:19 +0000

    Add deck image cacheing

--- server.js
+++ server.js
@@ -2,9 +2,8 @@
 "use strict";
 var qstring = require("querystring");
 var crypto = require("crypto");
-var connect = require("connect");
 var fs = require("fs");
-var app = require("http").createServer(connect().use(require("compression")()).use(cardRedirect).use(deckRedirect).use(require("serve-static")(__dirname)).use(loginAuth).use(codeSmith));
+var app = require("connect")().use(require("compression")()).use(require("serve-static")(__dirname)).use(cardRedirect).use(deckRedirect).use(loginAuth).use(codeSmith);
 var io = require("socket.io")(app.listen(13602));
 var db = require("redis").createClient();
 var etgutil = require("./etgutil");
@@ -144,42 +143,50 @@ function cardRedirect(req, res, next){
 function deckRedirect(req, res, next){
 	if (req.url.match(/^\/deck\//)){
 		var deck = req.url.substr(6);
-		var Canvas = require("canvas"), Image = Canvas.Image;
-		var can = new Canvas(616, 160), ctx = can.getContext("2d");
-		var elecols = [
-			"#a99683", "#aa5999", "#777777", "#996633", "#5f4930", "#50a005", "#cc6611", "#205080", "#a9a9a9", "#337ddd", "#ccaa22", "#333333", "#77bbdd",
-			"#d4cac1", "#d4accc", "#bbbbbb", "#ccb299", "#afa497", "#a7cf82", "#e5b288", "#8fa7bf", "#d4d4d4", "#99beee", "#e5d490", "#999999", "#bbddee"];
-		ctx.font = "11px Dosis";
-		ctx.textBaseline = "top";
-		var x=16, y=0;
-		etgutil.iterdeck(deck, function(code){
-			if (!(code in Cards.Codes)){
-				var ismark = etg.fromTrueMark(code);
-				if (~ismark){
-					ctx.fillStyle = elecols[ismark];
-					ctx.fillRect(0, 0, 16, 160);
-				}
-				return;
-			}
-			var card = Cards.Codes[code];
-			ctx.fillStyle = elecols[card.element+(card.upped?13:0)];
-			ctx.fillRect(x, y, 100, 16);
-			ctx.fillStyle = "#000000";
-			ctx.strokeRect(x, y, 100, 16);
-			ctx.fillText(card.name, x+2, y);
-			y += 16;
-			if (y == 160){
-				y=0;
-				x+=100;
-			}
-		});
-		can.toBuffer(function(err, buf){
-			if (err){
-				res.writeHead("503");
-				res.end();
-			}else{
+		fs.readFile(__dirname + "/deckcache/" + deck, function(err, data){
+			if (!err){
 				res.writeHead("200", {"Content-Type": "image/png"});
-				res.end(buf, "binary");
+				res.end(data, "binary");
+			}else{
+				var Canvas = require("canvas"), Image = Canvas.Image;
+				var can = new Canvas(616, 160), ctx = can.getContext("2d");
+				var elecols = [
+					"#a99683", "#aa5999", "#777777", "#996633", "#5f4930", "#50a005", "#cc6611", "#205080", "#a9a9a9", "#337ddd", "#ccaa22", "#333333", "#77bbdd",
+					"#d4cac1", "#d4accc", "#bbbbbb", "#ccb299", "#afa497", "#a7cf82", "#e5b288", "#8fa7bf", "#d4d4d4", "#99beee", "#e5d490", "#999999", "#bbddee"];
+				ctx.font = "11px Dosis";
+				ctx.textBaseline = "top";
+				var x=16, y=0;
+				etgutil.iterdeck(deck, function(code){
+					if (!(code in Cards.Codes)){
+						var ismark = etg.fromTrueMark(code);
+						if (~ismark){
+							ctx.fillStyle = elecols[ismark];
+							ctx.fillRect(0, 0, 16, 160);
+						}
+						return;
+					}
+					var card = Cards.Codes[code];
+					ctx.fillStyle = elecols[card.element+(card.upped?13:0)];
+					ctx.fillRect(x, y, 100, 16);
+					ctx.fillStyle = "#000000";
+					ctx.strokeRect(x, y, 100, 16);
+					ctx.fillText(card.name, x+2, y);
+					y += 16;
+					if (y == 160){
+						y=0;
+						x+=100;
+					}
+				});
+				can.toBuffer(function(err, buf){
+					if (err){
+						res.writeHead("503");
+						res.end();
+					}else{
+						res.writeHead("200", {"Content-Type": "image/png"});
+						res.end(buf, "binary");
+						fs.writeFile(__dirname + "/deckcache/" + deck, buf, {encoding: "binary"});
+					}
+				});
 			}
 		});
 	}else next();
