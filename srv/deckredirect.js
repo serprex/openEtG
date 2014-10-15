ffb10e96a01d4b8f361406f8c5d92399ad0f94a7
Demur Rumed <gunkmute@gmail.com>
2014-10-15 01:27:31 +0000

    Remove serverside canvas for svg

--- srv/deckredirect.js
+++ srv/deckredirect.js
@@ -6,50 +6,36 @@ function deckRedirect(req, res, next){
 	var deck = req.url.substr(1).replace(".png", "");
 	fs.readFile("deckcache/" + deck, function(err, data){
 		if (!err){
-			res.writeHead(200, {"Content-Type": "image/png"});
-			res.end(data, "binary");
+			res.writeHead(200, {"Content-Type": "image/svg+xml"});
+			res.end(data);
 		}else{
-			var Canvas = require("canvas"), Image = Canvas.Image;
-			var can = new Canvas(616, 160), ctx = can.getContext("2d");
+			var ret = "<svg xmlns='http://www.w3.org/2000/svg' width='616' height='160'>";
 			var elecols = [
 				"#a99683", "#aa5999", "#777777", "#996633", "#5f4930", "#50a005", "#cc6611", "#205080", "#a9a9a9", "#337ddd", "#ccaa22", "#333333", "#77bbdd",
 				"#d4cac1", "#d4accc", "#bbbbbb", "#ccb299", "#afa497", "#a7cf82", "#e5b288", "#8fa7bf", "#d4d4d4", "#99beee", "#e5d490", "#999999", "#bbddee"];
-			ctx.font = "10px Dosis";
-			ctx.textBaseline = "top";
 			var x=16, y=0;
 			etgutil.iterdeck(deck, function(code){
 				if (!(code in Cards.Codes)){
 					var ismark = etg.fromTrueMark(code);
 					if (~ismark){
-						ctx.fillStyle = elecols[ismark];
-						ctx.fillRect(0, 0, 16, 160);
+						ret += "<rect width='16' height='160' fill='"+elecols[ismark]+"'/>"
 					}
 					return;
 				}
 				var card = Cards.Codes[code];
-				ctx.beginPath();
-				ctx.rect(x, y, 100, 16);
-				ctx.fillStyle = elecols[card.element+(card.upped?13:0)];
-				ctx.fill();
-				ctx.stroke();
-				ctx.fillStyle = "#000000";
-				ctx.fillText(card.name, x+2, y);
+				ret += "<rect x='"+x+"' y='"+y+"' width='100' height='16' fill='"+elecols[card.element+(card.upped?13:0)]+"' stroke-width='.5' stroke='black'/>";
+				var textColor = card.upped ? "" : " fill='white'";
+				ret += "<text x='" + (x+2) + "' y='" + (y+13) + "'" + textColor + ">" + card.name + "</text>";
 				y += 16;
 				if (y == 160){
 					y=0;
 					x+=100;
 				}
 			});
-			can.toBuffer(function(err, buf){
-				if (err){
-					res.writeHead(503);
-					res.end();
-				}else{
-					res.writeHead(200, {"Content-Type": "image/png"});
-					res.end(buf, "binary");
-					fs.writeFile("deckcache/" + deck, buf, {encoding: "binary"}, function(){});
-				}
-			});
+			ret += "</svg>";
+			res.writeHead(200, {"Content-Type": "image/svg+xml"});
+			res.end(ret);
+			fs.writeFile("deckcache/" + deck, ret, function(){});
 		}
 	});
 }
