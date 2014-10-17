658e336e84122dc1755c60f0c5bf7e42292ce605
Demur Rumed <gunkmute@gmail.com>
2014-10-17 01:23:53 +0000

    Further reduce size of svg deck images

--- srv/deckredirect.js
+++ srv/deckredirect.js
@@ -2,9 +2,24 @@ var fs = require("fs");
 var etg = require("../etg");
 var Cards = require("../Cards");
 var etgutil = require("../etgutil");
+function eleChar(card){
+	return String.fromCharCode(97+card.element+(card.upped?13:0));
+}
 function deckRedirect(req, res, next){
 	var deck = req.url.substr(1).replace(".png", "");
 	fs.readFile("deckcache/" + deck, function(err, data){
+		function addClass(cls, style){
+			if (!(cls in classes)){
+				classes[cls] = style;
+			}
+		}
+		function classString(){
+			var ret = "";
+			for(var cls in classes){
+				ret += "."+cls+"{"+classes[cls]+"}";
+			}
+			return ret;
+		}
 		if (!err){
 			res.writeHead(200, {"Content-Type": "image/svg+xml"});
 			res.end(data);
@@ -12,25 +27,31 @@ function deckRedirect(req, res, next){
 			var elecols = [
 				"#a99683", "#aa5999", "#777777", "#996633", "#5f4930", "#50a005", "#cc6611", "#205080", "#a9a9a9", "#337ddd", "#ccaa22", "#333333", "#77bbdd",
 				"#d4cac1", "#d4accc", "#bbbbbb", "#ccb299", "#afa497", "#a7cf82", "#e5b288", "#8fa7bf", "#d4d4d4", "#99beee", "#e5d490", "#999999", "#bbddee"];
-			var ret="", x=16, y=0;
+			var ret="", x=16, y=0, classes = {};
 			etgutil.iterdeck(deck, function(code){
 				if (!(code in Cards.Codes)){
 					var ismark = etg.fromTrueMark(code);
 					if (~ismark){
-						ret += "<rect width='16' height='160' fill='"+elecols[ismark]+"'/>"
+						var cls = String.fromCharCode(97+ismark);
+						ret += "<rect class='"+cls+"' width='16' height='160'/>";
+						addClass(cls, "fill:"+elecols[ismark]);
 					}
 					return;
 				}
 				var card = Cards.Codes[code];
+				var elech = eleChar(card), elecls = (card.shiny?"A":"B") + " " + elech;
+				if (card.shiny) addClass("A", "stroke:#daa520;stroke-width:.5");
+				else addClass("B", "stroke:black;stroke-width:.5");
+				addClass(elech, "fill:"+elecols[card.element+(card.upped?13:0)]);
 				var textColor = card.upped ? "" : " fill='white'";
-				ret += "<rect x='"+x+"' y='"+y+"' width='100' height='16' fill='"+elecols[card.element+(card.upped?13:0)]+"' class='c'/><text x='" + (x+2) + "' y='" + (y+13) + "'" + textColor + ">" + card.name + "</text>";
+				ret += "<rect class='"+elecls+"' x='"+x+"' y='"+y+"' width='100' height='16'/><text x='" + (x+2) + "' y='" + (y+13) + "'" + textColor + ">" + card.name + "</text>";
 				y += 16;
 				if (y == 160){
 					y=0;
 					x+=100;
 				}
 			});
-			ret = "<svg xmlns='http://www.w3.org/2000/svg' height='160'"+" width='"+(y?x+100:x)+"'><style type='text/css'><![CDATA[text{font-size:12px;}.c{stroke:black;stroke-width:.5;}]]></style>" + ret + "</svg>";
+			ret = "<svg xmlns='http://www.w3.org/2000/svg' height='160'"+" width='"+(y?x+100:x)+"'><style type='text/css'><![CDATA[text{font-size:12px}"+classString()+"]]></style>" + ret + "</svg>";
 			res.writeHead(200, {"Content-Type": "image/svg+xml"});
 			res.end(ret);
 			fs.writeFile("deckcache/" + deck, ret, function(){});
