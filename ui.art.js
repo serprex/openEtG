c6cc058ecb20ad238f3328db0387862089c4aa57
Demur Rumed <gunkmute@gmail.com>
2014-11-12 04:03:52 +0000

    ravi, lin, timpa

--- ui.art.js
+++ ui.art.js
@@ -3,7 +3,7 @@ require("./httpcards")(function() {
 	var Cards = require("./Cards");
 	var credits = [
 		[["andretimpa", "http://andretimpa.deviantart.com"], ["Element Icons", "assets/esheet.png"], ["Rarity Icons", "assets/raritysheet.png"], ["Opening Music", "sound/openingMusic.ogg"],
-			["4sa", "4si", "4sj", "4sk", "4sl", "4sm", "4sn", "4so", "4sp", "4sq", "4sr", "4ss", "4st", "4su", "4vr", "55k", "55s", "6ub"]],
+			["4sa", "4si", "4sj", "4sk", "4sl", "4sm", "4sn", "4so", "4sp", "4sq", "4sr", "4ss", "4st", "4su", "4vr", "55k", "55s", "5rl", "6ub"]],
 		[["artimies", "http://elementscommunity.org/forum/profile/?u=140"], ["593"]],
 		[["Cryotube", "http://cryotube.deviantart.com"],
 			["566", "597", "5fe", "5lr"]],
@@ -13,6 +13,7 @@ require("./httpcards")(function() {
 		[["kae", "http://willowdream.carbonmade.com"], ["DeviantArt", "http://willowdream.deviantart.com"], ["Status Icons", "assets/statussheet.png"], ["Status Borders", "assets/statusborders.png"],
 			["4sd", "4sg", "4tc", "501", "52l", "52q", "535", "536", "55u", "567", "56b", "58s", "598", "59b", "59c", "59d", "59e", "5bs", "5bu", "5c2", "5c4", "5c6", "5c7", "5f5", "5fb", "5fk", "5fl", "5fm", "5fo", "5ib", "5il", "5io", "5iq", "5ld", "5lg", "5lj", "5ls", "5lt", "5od", "5oe", "5og",
 			"5ok", "5om", "5oo", "5oq", "5os", "5ov", "5p0", "5rg", "5rj", "5rr", "5s4", "5s8", "5uo", "5v2", "5v8", "5vf", "62b", "6rs", "7ae", "7ak", "77c", "7gr", "7h5", "7k0"]],
+		[["Lost in Nowhere", "http://etg.dek.im/forum/index.php?action=profile;u=38"], ["4vg"]],
 		[["mega plini", "http://elementscommunity.org/forum/profile/?u=202"], ["5ig"]],
 		[["moomoose", "http://elementscommunity.org/forum/profile/?u=40"], ["5i6"]],
 		[["OdinVanguard", "http://elementscommunity.org/forum/profile/?u=232"],
@@ -22,8 +23,8 @@ require("./httpcards")(function() {
 			["52g", "58o", "5bv", "5f0", "5i4", "5ie", "5l8", "5lb", "5oj"]],
 		[["Ravizant", "http://elementscommunity.org/forum/profile/?u=8037"], ["Element Icons", "assets/esheet.png"], ["Card Backgrounds", "assets/backsheet.png"],
 			["4sc", "4tb", "4vp", "4vi", "4vj", "4vq", "4vt", "4vu", "50a", "50u", "52h", "52k", "52v", "53e", "542", "55q", "55v", "564", "56i", "576", "58p", "58q", "599", "59m", "5c3", "5cb", "5ce", "5cf", "5cg", "5cq", "5de", "5f3", "5f8", "5f9", "5fd", "5fi", "5fu", "5gi", "5ia",
-			"5ih", "5ik", "5im", "5ip", "5j2", "5jm", "5l9", "5lc", "5lh", "5lp", "5m6", "5mq", "5oh", "5ou", "5pa", "5pu", "5rn", "5rq", "5se", "5t2", "5uq", "5uu", "5vi", "61p", "61q", "61v", "620", "627", "62m", "6rr", "6u3", "77a", "77p", "7au", "7av", "7dj", "7e2", "7k1", "7qa", "7ta", "809", "80a"]],
-		[["serprex", "http://fiction.wikia.com/wiki/User:Serprex"], ["58t", "5rl", "5ro", "5rp", "5s0", "5va", "622", "62a"]],
+			"5ih", "5ik", "5im", "5ip", "5j2", "5jm", "5l9", "5la", "5lc", "5lh", "5lp", "5m6", "5mq", "5oh", "5ou", "5pa", "5pu", "5rn", "5rq", "5se", "5t2", "5uq", "5uu", "5vi", "61p", "61q", "61v", "620", "627", "62m", "6rr", "6u3", "77a", "77p", "7au", "7av", "7dj", "7e2", "7jq", "7k1", "7qa", "7ta", "809", "80a"]],
+		[["serprex", "http://fiction.wikia.com/wiki/User:Serprex"], ["58t", "lrl", "5ro", "5rp", "5s0", "5va", "622", "62a"]],
 		[["Thalas", "http://elementscommunity.org/forum/profile/?u=103"], ["5i9", "5if", "7dl"]],
 		[["vrt", "http://vrt-designs.com"], ["Donation thread", "http://elementscommunity.org/forum/card-art/help-support-an-artist"],
 			["4sb", "4vc", "4ve", "4vh", "52t", "55l", "55o", "55r", "560", "562", "563", "591", "5c0", "5c1", "5c9", "5f1", "5f2", "5fa", "5fc", "5i5", "5i7", "5id", "5ij", "5ll", "5oc", "5of", "5rk", "5rs", "5rt", "5uk", "5ul", "5um", "5ut", "5uv", "5v3", "61o", "61t", "624", "625", "626", "74a", "80g"]],
