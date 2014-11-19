eb5d13ee9ac7c8a537c4aaf55e34ace73457690e
Demur Rumed <gunkmute@gmail.com>
2014-11-19 19:40:30 +0000

    Spell out mark

--- srv/deckredirect.js
+++ srv/deckredirect.js
@@ -27,15 +27,11 @@ function deckRedirect(req, res, next){
 			var elecols = [
 				"#a99683", "#aa5999", "#777777", "#996633", "#5f4930", "#50a005", "#cc6611", "#205080", "#a9a9a9", "#337ddd", "#ccaa22", "#333333", "#77bbdd",
 				"#d4cac1", "#d4accc", "#bbbbbb", "#ccb299", "#afa497", "#a7cf82", "#e5b288", "#8fa7bf", "#d4d4d4", "#99beee", "#e5d490", "#999999", "#bbddee"];
-			var ret="", x=16, y=0, classes = {};
+			var ret="", x=16, y=0, classes = {}, mark;
 			etgutil.iterdeck(deck, function(code){
 				if (!(code in Cards.Codes)){
 					var ismark = etg.fromTrueMark(code);
-					if (~ismark){
-						var cls = String.fromCharCode(97+ismark);
-						ret += "<rect class='"+cls+"' width='16' height='160'/>";
-						addClass(cls, "fill:"+elecols[ismark]);
-					}
+					if (~ismark) mark = ismark;
 					return;
 				}
 				var card = Cards.Codes[code];
@@ -51,7 +47,12 @@ function deckRedirect(req, res, next){
 					x+=100;
 				}
 			});
-			ret = "<svg xmlns='http://www.w3.org/2000/svg' height='160'"+" width='"+(y?x+100:x)+"'><style type='text/css'><![CDATA[text{font-size:12px}"+classString()+"]]></style>" + ret + "</svg>";
+			if (mark !== undefined){
+				var cls = String.fromCharCode(97+mark);
+				ret += "<rect class='"+cls+"' width='16' height='160'/><text x='5' y='-5' transform='rotate(90)'>"+etg.eleNames[mark]+"</text>";
+				addClass(cls, "fill:"+elecols[mark]);
+			}
+			ret = "<svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' height='160'"+" width='"+(y?x+100:x)+"'><style type='text/css'><![CDATA[text{font-size:12px}"+classString()+"]]></style>" + ret + "</svg>";
 			res.writeHead(200, {"Content-Type": "image/svg+xml"});
 			res.end(ret);
 			fs.writeFile("deckcache/" + deck, ret, function(){});
