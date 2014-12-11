e0f2b73210435aa01acc295c46de7b1765fa20b3
Demur Rumed <gunkmute@gmail.com>
2014-12-11 21:13:52 +0000

    3rd time's a charm

--- views/Reward.js
+++ views/Reward.js
@@ -15,8 +15,7 @@ module.exports = function(reward, numberofcopies, code) {
 		if (shiny) reward = reward.slice(1);
 		var upped = reward.slice(0, 5) == "upped";
 		var rarity = userutil.rewardwords[upped ? reward.slice(5) : reward];
-		rewardList = etg.filtercards(upped, function(x) { return x.rarity == rarity }).map(function(card){ return card.code });
-		if (shiny) rewardList = rewardList.map(function(x) { return x.asShiny(true) });
+		rewardList = etg.filtercards(upped, function(x) { return x.rarity == rarity }).map(function(card){ return card.asShiny(shiny).code });
 	}else if (reward instanceof Array){
 		rewardList = reward;
 	}else{
