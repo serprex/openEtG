2328fc856f567b9ab7188adcb54a2e54fe4cd8d0
Demur Rumed <gunkmute@gmail.com>
2015-01-31 04:37:53 +0000

    Use px.domEButton for views/ElementSelect Also have exiting ElementSelect go to Login, not MainMenu

--- views/ElementSelect.js
+++ views/ElementSelect.js
@@ -4,30 +4,27 @@ var etg = require("../etg");
 var gfx = require("../gfx");
 var sock = require("../sock");
 module.exports = function() {
-	var stage = px.mkView();
-	var eledesc = px.domText("Select your starter element");
+	var eledesc = px.domText("Select your starter element"),
+		dom = [
+		[100, 250, eledesc],
+		[100, 400, ["Exit", function(){
+			sock.userEmit("delete");
+			sock.user = undefined;
+			require("./Login")();
+		}]]
+	];
 	etg.eleNames.forEach(function(name, i){
 		if (i > 13) return;
-		var ele = new PIXI.Sprite(gfx.eicons[i]);
-		ele.position.set(100 + i * 32, 300);
-		ele.mouseover = function(){
-			eledesc.text = name;
-		}
-		px.setClick(ele, function() {
+		var b = px.domEButton(i, function() {
 			var msg = { u: sock.user.name, a: sock.user.auth, e: i };
 			sock.user = undefined;
 			sock.emit("inituser", msg);
 			require("./MainMenu")();
 		});
-		ele.interactive = true;
-		stage.addChild(ele);
+		b.addEventListener("mouseover", function(){
+			eledesc.text = name;
+		});
+		dom.push([100 + i * 32, 300, b]);
 	});
-	px.refreshRenderer({view: stage, seldom: [
-		[100, 250, eledesc],
-		[100, 400, ["Exit", function(){
-			sock.userEmit("delete");
-			sock.user = undefined;
-			require("./MainMenu")();
-		}]
-	]]});
+	px.refreshRenderer({seldom: dom});
 }
