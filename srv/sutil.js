d2ce9103e462ba4623ad62fc3a39b013e7764057
Demur Rumed <gunkmute@gmail.com>
2015-02-21 03:04:27 +0000

    Create a mkTask method for server to batch async calls together

--- srv/sutil.js
+++ srv/sutil.js
@@ -15,33 +15,56 @@ exports.prepuser = function(servuser){
 		servuser[field] = parseInt(servuser[field] || 0);
 	});
 }
+exports.mkTask = function(cb){
+	var params = {}, cbCount = 1;
+	function cbCheck(){
+		if (--cbCount == 0){
+			cb(params);
+		}
+	}
+	return function(param){
+		if (arguments.length == 0){
+			cbCheck();
+		}else{
+			cbCount++;
+			return function(err, res){
+				params[param] = res;
+				if (err){
+					if (!params.err) params.err = {};
+					params.err[param] = err;
+				}
+				cbCheck();
+			}
+		}
+	}
+}
 exports.useruser = function(db, servuser, cb){
-	db.hgetall("Q:" + servuser.name, function(err, quest) {
-		db.hgetall("D:" + servuser.name, function(err, decks) {
-			db.lrange("N:" + servuser.name, 0, 9, function(err, quickdecks) {
-				cb({
-					auth: servuser.auth,
-					name: servuser.name,
-					decknames: decks,
-					selectedDeck: servuser.selectedDeck,
-					pool: servuser.pool,
-					accountbound: servuser.accountbound,
-					gold: servuser.gold,
-					ocard: servuser.ocard,
-					freepacks: servuser.freepacks,
-					aiwins: servuser.aiwins,
-					ailosses: servuser.ailosses,
-					pvpwins: servuser.pvpwins,
-					pvplosses: servuser.pvplosses,
-					daily: servuser.daily,
-					dailymage: servuser.dailymage,
-					dailydg: servuser.dailydg,
-					quest: quest,
-					quickdecks: quickdecks,
-				});
-			});
+	var task = exports.mkTask(function(results){
+		cb({
+			auth: servuser.auth,
+			name: servuser.name,
+			selectedDeck: servuser.selectedDeck,
+			pool: servuser.pool,
+			accountbound: servuser.accountbound,
+			gold: servuser.gold,
+			ocard: servuser.ocard,
+			freepacks: servuser.freepacks,
+			aiwins: servuser.aiwins,
+			ailosses: servuser.ailosses,
+			pvpwins: servuser.pvpwins,
+			pvplosses: servuser.pvplosses,
+			daily: servuser.daily,
+			dailymage: servuser.dailymage,
+			dailydg: servuser.dailydg,
+			decknames: results.decks,
+			quest: results.quest,
+			quickdecks: results.quickdecks,
 		});
 	});
+	db.hgetall("Q:" + servuser.name, task("quest"));
+	db.hgetall("D:" + servuser.name, task("decks"));
+	db.lrange("N:" + servuser.name, 0, 9, task("quickdecks"));
+	task();
 }
 exports.getDay = function(){
 	return Math.floor(Date.now()/86400000);
