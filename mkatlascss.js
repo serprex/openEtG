f9d8a00be68873c3f58ff376e06f3b2890c7204a
Demur Rumed <gunkmute@gmail.com>
2015-03-15 22:49:27 +0000

    mkatlascss update, should really grab width/height from image file

--- mkatlascss.js
+++ mkatlascss.js
@@ -7,7 +7,7 @@ for(var asset in assets){
 	var data = assets[asset];
 	out.write("."+asset+"{width:"+data[2]+"px;height:"+data[3]+"px;background-position:-"+data[0]+"px -"+data[1]+"px}\n");
 	if (asset.match(/e\d+/)){
-		out.write(".c"+asset+"{background-size:208.5px 160px;width:"+data[2]/2+"px;height:"+data[3]/2+"px;background-position:-"+data[0]/2+"px -"+data[1]/2+"px}\n");
+		out.write(".c"+asset+"{background-size:234px 177.5px;width:"+data[2]/2+"px;height:"+data[3]/2+"px;background-position:-"+data[0]/2+"px -"+data[1]/2+"px}\n");
 	}
 }
 out.end();
