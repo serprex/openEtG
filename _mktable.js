994f5edba6701ffc01732592e0894a01fbb6c3da
Demur Rumed <gunkmute@gmail.com>
2015-06-09 00:50:16 +0000

    Update _mktable with mage/dg param

--- _mktable.js
+++ _mktable.js
@@ -1,29 +1,38 @@
 #!/usr/bin/node
 "use strict";
-require("./srv/sutil").loadcards(function(){
-	var etg = require("./etg");
-	var write = process.stdout.write.bind(process.stdout);
-	function writetd(){
-		write("[tr]");
-		for(var i=0; i<arguments.length; i++)
-			if (arguments[i] !== undefined) write("[td]"+arguments[i]+"[/td]");
-		write("[/tr]");
-	}
-	write("[right][table]");
-	writetd("Tot", "Ele", "C", "P", "S", "|", "R", "U", "C", "", "");
-	for(var i=0; i<13; i++){
-		var ofele = etg.filtercards(false, function(x){return x.element == i});
-		var creas = 0, perms = 0, spels = 0, comm = [0, 0, 0], last = "000";
-		ofele.forEach(function(x){
-			if (x.type <= etg.PermanentEnum) perms++;
-			else if (x.type == etg.CreatureEnum) creas++;
-			else if (x.type == etg.SpellEnum) spels++;
-			if (x.rarity > 0 && x.rarity < 4){
-				comm[x.rarity-1]++;
-				if (x.code > last) last = x.code;
-			}
+var write = process.stdout.write.bind(process.stdout);
+if (process.argv.length < 3){
+	require("./srv/sutil").loadcards(function(){
+		var etg = require("./etg");
+		function writetd(){
+			write("[tr]");
+			for(var i=0; i<arguments.length; i++)
+				if (arguments[i] !== undefined) write("[td]"+arguments[i]+"[/td]");
+			write("[/tr]");
+		}
+		write("[right][table]");
+		writetd("Tot", "Ele", "C", "P", "S", "|", "R", "U", "C", "", "");
+		for(var i=0; i<13; i++){
+			var ofele = etg.filtercards(false, function(x){return x.element == i});
+			var creas = 0, perms = 0, spels = 0, comm = new Uint32Array(3), last = "000";
+			ofele.forEach(function(x){
+				if (x.type <= etg.PermanentEnum) perms++;
+				else if (x.type == etg.CreatureEnum) creas++;
+				else if (x.type == etg.SpellEnum) spels++;
+				if (x.rarity > 0 && x.rarity < 4){
+					comm[x.rarity-1]++;
+					if (x.code > last) last = x.code;
+				}
+			});
+			writetd(ofele.length, etg.eleNames[i], creas, perms, spels, "|", comm[2], comm[1], comm[0], last, (parseInt(last, 32)+2000).toString(32));
+		}
+		write("[/table][/right]\n");
+	});
+}else{
+	var decks = require("./Decks")[process.argv[2]];
+	if (decks){
+		decks.forEach(function(deck){
+			write("[deck title="+deck[0]+"]"+deck[1]+"[/deck]");
 		});
-		writetd(ofele.length, etg.eleNames[i], creas, perms, spels, "|", comm[2], comm[1], comm[0], last, (parseInt(last, 32)+2000).toString(32));
 	}
-	write("[/table][/right]\n");
-});
+}
