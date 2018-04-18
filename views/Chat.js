a36b20fbb1383a12bbb9ca11b0aca6f1f2690a5b
Demur Rumed <gunkmute@gmail.com>
2018-04-18 01:28:02 +0000

    Fix autoscroll

--- views/Chat.js
+++ views/Chat.js
@@ -3,8 +3,28 @@ const {connect} = require('react-redux'),
 
 module.exports = connect(state => ({
 	chat: state.chat,
-}))(function Chat(props) {
-	return <div className='chatBox' style={props.style}>
-		{props.chat.get(props.channel)}
-	</div>;
+}))(class Chat extends React.Component {
+	constructor(props) {
+		super(props);
+		this.chatRef = React.createRef();
+	}
+
+	getSnapshotBeforeUpdate(prevProps, prevState) {
+		const chat = this.chatRef.current;
+		if (prevProps.channel !== this.props.channel || Math.abs(chat.scrollTop - chat.scrollHeight + chat.offsetHeight) < 2) {
+			return -1;
+		}
+		return chat.scrollTop;
+	}
+
+	componentDidUpdate(prevProps, prevState, snapshot) {
+		const chat = this.chatRef.current;
+		chat.scrollTop = ~snapshot ? snapshot : chat.scrollHeight;
+	}
+
+	render() {
+		return <div className='chatBox' style={this.props.style} ref={this.chatRef}>
+			{this.props.chat.get(this.props.channel)}
+		</div>;
+	}
 })
