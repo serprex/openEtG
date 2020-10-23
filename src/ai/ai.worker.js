ed1aa70c4f4d6efdbfaea132068097a4f0b88bc4
Philip Dubé <gunkmute@gmail.com>
2020-10-23 04:12:03 +0000

    Greatly increase AI speed by having worker cache move list to avoid replaying all moves each move

--- src/ai/ai.worker.js
+++ src/ai/ai.worker.js
@@ -3,14 +3,25 @@ Effect.disable = true;
 import Game from '../Game.js';
 import aiSearch from './search.js';
 
+let game = null,
+	premoves = null;
 onmessage = function (e) {
 	const {
 		id,
 		data: { data, moves },
 	} = e.data;
-	const game = new Game(data);
-	for (const move of moves) {
-		game.next(move);
+	if (
+		game === null ||
+		premoves.length > moves.length ||
+		JSON.stringify(data) !== JSON.stringify(game.data) ||
+		JSON.stringify(premoves) !== JSON.stringify(moves.slice(0, premoves.length))
+	) {
+		game = new Game(data);
+		premoves = null;
 	}
+	for (let i = premoves !== null ? premoves.length : 0; i < moves.length; i++) {
+		game.next(moves[i]);
+	}
+	premoves = moves;
 	postMessage({ id, cmd: aiSearch(game) });
 };
