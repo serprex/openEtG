a1ae7454d78b144fab35515fa3d7fa1cf9b6441c
Demur Rumed <junkmute@hotmail.com>
2023-05-15 22:44:25 +0000

    Port vanilla Bazaar/Upgrade to hooks

--- src/vanilla/views/Bazaar.jsx
+++ src/vanilla/views/Bazaar.jsx
@@ -1,5 +1,5 @@
-import { Component } from 'react';
-import { connect } from 'react-redux';
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
 
 import * as etgutil from '../../etgutil.js';
 import Cards from '../Cards.js';
@@ -7,113 +7,103 @@ import { userEmit } from '../../sock.jsx';
 import * as store from '../../store.jsx';
 import * as Components from '../../Components/index.jsx';
 
-export default connect(({ user, orig }) => ({ user, orig }))(
-	class OriginalBazaar extends Component {
-		state = { deck: [] };
+export default function OriginalBazaar(props) {
+	const user = useSelector(({ user }) => user);
+	const orig = useSelector(({ orig }) => orig);
+	const [deck, setDeck] = useState([]);
+	const [card, setCard] = setState(null);
 
-		render() {
-			let cost = 0;
-			for (const code of this.state.deck) {
-				const card = Cards.Codes[code];
-				cost += 6 * card.rarity ** 2 + card.cost;
-			}
+	let cost = 0;
+	for (const code of deck) {
+		const card = Cards.Codes[code];
+		cost += 6 * card.rarity ** 2 + card.cost;
+	}
 
-			return (
-				<>
-					<Components.DeckDisplay
-						cards={Cards}
-						deck={this.state.deck}
-						onMouseOver={(i, card) => this.setState({ card })}
-						onClick={i => {
-							const newdeck = this.state.deck.slice();
-							newdeck.splice(i, 1);
-							this.setState({ deck: newdeck });
-						}}
-					/>
-					<Components.Text
-						text={`${this.props.orig.electrum}$`}
-						style={{
-							position: 'absolute',
-							left: '8px',
-							top: '235px',
-						}}
-					/>
-					<Components.Text
-						text={`${cost}$`}
-						style={{
-							position: 'absolute',
-							left: '100px',
-							top: '235px',
-						}}
-					/>
-					{this.state.deck.length > 0 &&
-						(this.props.orig.electrum >= cost ? (
-							<input
-								type="button"
-								value="Buy"
-								style={{
-									position: 'absolute',
-									left: '200px',
-									top: '235px',
-								}}
-								onClick={() => {
-									const update = {
-										electrum: -cost,
-										pool: etgutil.encodedeck(this.state.deck),
-									};
-									userEmit('origadd', update);
-									this.props.dispatch(store.addOrig(update));
-
-									this.props.dispatch(store.doNav(import('./MainMenu.jsx')));
-								}}
-							/>
-						) : (
-							<div
-								style={{
-									position: 'absolute',
-									left: '200px',
-									top: '235px',
-								}}>
-								{`You need
-								${cost - this.props.orig.electrum} more electrum to afford ${
-									this.state.deck.length === 1 ? 'this card' : 'these cards'
-								}`}
-							</div>
-						))}
+	return (
+		<>
+			<Components.DeckDisplay
+				cards={Cards}
+				deck={deck}
+				onMouseOver={(i, card) => setCard(card)}
+				onClick={i => {
+					const newdeck = deck.slice();
+					newdeck.splice(i, 1);
+					setDeck(newdeck);
+				}}
+			/>
+			<Components.Text
+				text={`${orig.electrum}$`}
+				style={{
+					position: 'absolute',
+					left: '8px',
+					top: '235px',
+				}}
+			/>
+			<Components.Text
+				text={`${cost}$`}
+				style={{
+					position: 'absolute',
+					left: '100px',
+					top: '235px',
+				}}
+			/>
+			{deck.length > 0 &&
+				(orig.electrum >= cost ? (
 					<input
 						type="button"
-						value="Exit"
-						onClick={() => {
-							this.props.dispatch(store.doNav(import('./MainMenu.jsx')));
-						}}
+						value="Buy"
 						style={{
 							position: 'absolute',
-							left: '10px',
-							top: '10px',
+							left: '200px',
+							top: '235px',
+						}}
+						onClick={() => {
+							const update = {
+								electrum: -cost,
+								pool: etgutil.encodedeck(deck),
+							};
+							userEmit('origadd', update);
+							store.store.dispatch(store.addOrig(update));
+							store.store.dispatch(store.doNav(import('./MainMenu.jsx')));
 						}}
 					/>
-					<Components.CardSelector
-						cards={Cards}
-						filter={card =>
-							card.rarity >= 1 && card.rarity <= 4 && card.name !== 'Relic'
-						}
-						onMouseOver={card => this.setState({ card })}
-						onClick={card => {
-							const code = card.code;
-							if (
-								this.state.deck.length < 60 &&
-								!card.upped &&
-								!card.isFree()
-							) {
-								this.setState({
-									deck: this.state.deck.concat([code]),
-								});
-							}
-						}}
-					/>
-					<Components.Card x={734} y={8} card={this.state.card} />
-				</>
-			);
-		}
-	},
-);
+				) : (
+					<div
+						style={{
+							position: 'absolute',
+							left: '200px',
+							top: '235px',
+						}}>
+						{`You need ${cost - orig.electrum} more electrum to afford ${
+							deck.length === 1 ? 'this card' : 'these cards'
+						}`}
+					</div>
+				))}
+			<input
+				type="button"
+				value="Exit"
+				onClick={() => {
+					store.store.dispatch(store.doNav(import('./MainMenu.jsx')));
+				}}
+				style={{
+					position: 'absolute',
+					left: '10px',
+					top: '10px',
+				}}
+			/>
+			<Components.CardSelector
+				cards={Cards}
+				filter={card =>
+					card.rarity >= 1 && card.rarity <= 4 && card.name !== 'Relic'
+				}
+				onMouseOver={setCard}
+				onClick={card => {
+					if (deck.length < 60 && !card.upped && !card.isFree()) {
+						setDeck(deck.concat([card.code]));
+					}
+				}}
+			/>
+			<Components.Card x={734} y={8} card={card} />
+		</>
+	);
+}
--- src/vanilla/views/Upgrade.jsx
+++ src/vanilla/views/Upgrade.jsx
@@ -1,5 +1,5 @@
-import { Component } from 'react';
-import { connect } from 'react-redux';
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
 
 import * as etgutil from '../../etgutil.js';
 import Cards from '../Cards.js';
@@ -7,125 +7,115 @@ import { userEmit } from '../../sock.jsx';
 import * as store from '../../store.jsx';
 import * as Components from '../../Components/index.jsx';
 
-export default connect(({ user, orig }) => ({ user, orig }))(
-	class OriginalUpgrade extends Component {
-		state = { deck: [] };
+export default function OriginalUpgrade(props) {
+	const user = useSelector(({ user }) => user);
+	const orig = useSelector(({ orig }) => orig);
+	const [deck, setDeck] = useState([]);
+	const [card, setCard] = setState(null);
 
-		render() {
-			const cardminus = [];
-			for (const code of this.state.deck) {
-				cardminus[code] = (cardminus[code] ?? 0) + 1;
-			}
-			const cardpool = etgutil.deck2pool(this.props.orig.pool);
-			for (const key in cardpool) {
-				const card = Cards.Codes[key];
-				if (card.upped) delete cardpool[key];
-			}
-			const cost = this.state.deck.length * 1500;
+	const cardminus = [];
+	for (const code of deck) {
+		cardminus[code] = (cardminus[code] ?? 0) + 1;
+	}
+	const cardpool = etgutil.deck2pool(orig.pool);
+	for (const key in cardpool) {
+		const card = Cards.Codes[key];
+		if (card.upped) delete cardpool[key];
+	}
+	const cost = deck.length * 1500;
 
-			return (
-				<>
-					<Components.DeckDisplay
-						cards={Cards}
-						deck={this.state.deck}
-						onMouseOver={(i, card) => this.setState({ card })}
-						onClick={i => {
-							const newdeck = this.state.deck.slice();
-							newdeck.splice(i, 1);
-							this.setState({ deck: newdeck });
-						}}
-					/>
-					<Components.Text
-						text={`${this.props.orig.electrum}$`}
-						style={{
-							position: 'absolute',
-							left: '8px',
-							top: '235px',
-						}}
-					/>
-					<Components.Text
-						text={`${cost}$`}
-						style={{
-							position: 'absolute',
-							left: '100px',
-							top: '235px',
-						}}
-					/>
-					{this.state.deck.length > 0 &&
-						(this.props.orig.electrum >= cost ? (
-							<input
-								type="button"
-								value="Upgrade"
-								style={{
-									position: 'absolute',
-									left: '200px',
-									top: '235px',
-								}}
-								onClick={() => {
-									const update = {
-										electrum: -cost,
-										pool: etgutil.encodedeck(
-											this.state.deck.map(code => code + 2000),
-										),
-										rmpool: etgutil.encodedeck(
-											this.state.deck.filter(
-												code => !Cards.Codes[code].isFree(),
-											),
-										),
-									};
-									userEmit('origadd', update);
-									this.props.dispatch(store.addOrig(update));
-
-									this.props.dispatch(store.doNav(import('./MainMenu.jsx')));
-								}}
-							/>
-						) : (
-							<div
-								style={{
-									position: 'absolute',
-									left: '200px',
-									top: '235px',
-								}}>
-								{`You need
-								${cost - this.props.orig.electrum} more electrum to afford ${
-									this.state.deck.length === 1
-										? 'this upgrade'
-										: 'these upgrades'
-								}`}
-							</div>
-						))}
+	return (
+		<>
+			<Components.DeckDisplay
+				cards={Cards}
+				deck={deck}
+				onMouseOver={(i, card) => setCard(card)}
+				onClick={i => {
+					const newdeck = deck.slice();
+					newdeck.splice(i, 1);
+					setDeck(newdeck);
+				}}
+			/>
+			<Components.Text
+				text={`${orig.electrum}$`}
+				style={{
+					position: 'absolute',
+					left: '8px',
+					top: '235px',
+				}}
+			/>
+			<Components.Text
+				text={`${cost}$`}
+				style={{
+					position: 'absolute',
+					left: '100px',
+					top: '235px',
+				}}
+			/>
+			{deck.length > 0 &&
+				(orig.electrum >= cost ? (
 					<input
 						type="button"
-						value="Exit"
-						onClick={() => {
-							this.props.dispatch(store.doNav(import('./MainMenu.jsx')));
-						}}
+						value="Upgrade"
 						style={{
 							position: 'absolute',
-							left: '10px',
-							top: '10px',
+							left: '200px',
+							top: '235px',
+						}}
+						onClick={() => {
+							const update = {
+								electrum: -cost,
+								pool: etgutil.encodedeck(deck.map(code => code + 2000)),
+								rmpool: etgutil.encodedeck(
+									deck.filter(code => !Cards.Codes[code].isFree()),
+								),
+							};
+							userEmit('origadd', update);
+							store.store.dispatch(store.addOrig(update));
+							store.store.dispatch(store.doNav(import('./MainMenu.jsx')));
 						}}
 					/>
-					<Components.CardSelector
-						cards={Cards}
-						cardpool={cardpool}
-						cardminus={cardminus}
-						filter={card =>
-							card.isFree() ||
-							(card.code in cardpool &&
-								(cardminus[card.code] ?? 0) < cardpool[card.code])
-						}
-						onMouseOver={card => this.setState({ card })}
-						onClick={card => {
-							const code = card.code;
-							if (this.state.deck.length < 60) {
-								this.setState({ deck: this.state.deck.concat([code]) });
-							}
-						}}
-					/>
-					<Components.Card x={734} y={8} card={this.state.card} />
-				</>
-			);
-		}
-	},
-);
+				) : (
+					<div
+						style={{
+							position: 'absolute',
+							left: '200px',
+							top: '235px',
+						}}>
+						{`You need ${cost - orig.electrum} more electrum to afford ${
+							deck.length === 1 ? 'this upgrade' : 'these upgrades'
+						}`}
+					</div>
+				))}
+			<input
+				type="button"
+				value="Exit"
+				onClick={() => {
+					store.store.dispatch(store.doNav(import('./MainMenu.jsx')));
+				}}
+				style={{
+					position: 'absolute',
+					left: '10px',
+					top: '10px',
+				}}
+			/>
+			<Components.CardSelector
+				cards={Cards}
+				cardpool={cardpool}
+				cardminus={cardminus}
+				filter={card =>
+					card.isFree() ||
+					(card.code in cardpool &&
+						(cardminus[card.code] ?? 0) < cardpool[card.code])
+				}
+				onMouseOver={setCard}
+				onClick={card => {
+					if (deck.length < 60) {
+						setDeck(deck.concat([card.code]));
+					}
+				}}
+			/>
+			<Components.Card x={734} y={8} card={card} />
+		</>
+	);
+}
