60a69ef8bfabf2b51fea799cbf4b2a4cc4a64a9a
Demur Rumed <junkmute@hotmail.com>
2023-06-04 19:32:36 +0000

    Port legacy editor to react hooks

--- src/vanilla/views/Editor.jsx
+++ src/vanilla/views/Editor.jsx
@@ -1,5 +1,5 @@
-import { createRef, Component } from 'react';
-import { connect } from 'react-redux';
+import { useEffect, useMemo, useRef, useState } from 'react';
+import { useSelector } from 'react-redux';
 
 import { parseDeck } from './MainMenu.jsx';
 import * as etg from '../../etg.js';
@@ -11,134 +11,108 @@ import * as store from '../../store.jsx';
 import * as util from '../../util.js';
 import { userEmit } from '../../sock.jsx';
 
-function processDeck(pool, dcode) {
-	let mark = 0,
-		deck = etgutil.decodedeck(dcode);
-	for (let i = deck.length - 1; i >= 0; i--) {
-		if (!Cards.Codes[deck[i]]) {
-			const index = etgutil.fromTrueMark(deck[i]);
-			if (~index) {
-				mark = index;
+export default function OriginalEditor(props) {
+	const orig = useSelector(({ orig }) => orig);
+	const pool = useMemo(() => {
+		const pool = [];
+		for (const [code, count] of etgutil.iterraw(orig.pool)) {
+			if (Cards.Codes[code]) {
+				pool[code] = (pool[code] ?? 0) + count;
 			}
-			deck.splice(i, 1);
 		}
-	}
-	deck.sort(Cards.codeCmp).splice(60);
-	const cardMinus = Cards.filterDeck(deck, pool, true);
-	return { mark, deck, cardMinus };
-}
-
-export default connect(({ orig }) => ({ orig }))(
-	class OriginalEditor extends Component {
-		constructor(props) {
-			super(props);
-
-			const pool = [];
-			for (const [code, count] of etgutil.iterraw(props.orig.pool)) {
-				if (Cards.Codes[code]) {
-					pool[code] = (pool[code] ?? 0) + count;
+		return pool;
+	}, [orig.pool]);
+	const { mark, deck, cardMinus, deckCode } = useMemo(() => {
+		let mark = 0,
+			deck = etgutil.decodedeck(orig.deck);
+		for (let i = deck.length - 1; i >= 0; i--) {
+			if (!Cards.Codes[deck[i]]) {
+				const index = etgutil.fromTrueMark(deck[i]);
+				if (~index) {
+					mark = index;
 				}
+				deck.splice(i, 1);
 			}
-
-			this.deckRef = createRef();
-			this.state = {
-				pool: pool,
-				selectedDeck: '',
-				deck: [],
-				mark: 0,
-			};
 		}
+		deck.sort(Cards.codeCmp).splice(60);
+		const cardMinus = Cards.filterDeck(deck, pool, true),
+			deckCode = etgutil.encodedeck(deck) + etgutil.toTrueMarkSuffix(mark);
 
-		static getDerivedStateFromProps(nextProps, prevState) {
-			if (nextProps.orig.deck === prevState.selectedDeck) return null;
-			return {
-				selectedDeck: nextProps.orig.deck,
-				...processDeck(prevState.pool, nextProps.orig.deck),
-			};
-		}
+		return { mark, deck, cardMinus, deckCode };
+	}, [orig.deck, pool]);
 
-		setCardArt = card => {
-			if (this.state.card !== card) this.setState({ card });
-		};
+	const deckRef = useRef();
+	useEffect(() => {
+		deckRef.current.setSelectionRange(0, 999);
+	}, []);
 
-		componentDidMount() {
-			this.deckRef.current.setSelectionRange(0, 999);
-		}
-
-		currentDeckCode() {
-			return (
-				etgutil.encodedeck(this.state.deck) +
-				etgutil.toTrueMarkSuffix(this.state.mark)
-			);
-		}
-
-		saveDeck = () => {
-			const update = { deck: this.currentDeckCode() };
-			userEmit('updateorig', update);
-			this.props.dispatch(store.updateOrig(update));
-		};
-
-		render() {
-			const { deck, mark, cardMinus } = this.state;
-			return (
-				<>
-					<Editor
-						cards={Cards}
-						deck={this.state.deck}
-						mark={this.state.mark}
-						pool={this.state.pool}
-						cardMinus={this.state.cardMinus}
-						setDeck={deck => {
-							deck.sort(Cards.codeCmp);
-							const cardMinus = Cards.filterDeck(deck, this.state.pool, true);
-							this.setState({ deck, cardMinus });
-						}}
-						setMark={mark => this.setState({ mark })}
-					/>
-					<input
-						type="button"
-						value="Exit"
-						onClick={() => {
-							this.saveDeck();
-							this.props.dispatch(store.doNav(import('./MainMenu.jsx')));
-						}}
-						style={{
-							position: 'absolute',
-							left: '8px',
-							top: '110px',
-						}}
-					/>
-					<label
-						style={{
-							position: 'absolute',
-							left: '536px',
-							top: '238px',
-						}}>
-						Deck&nbsp;
-						<input
-							autoFocus
-							value={this.currentDeckCode()}
-							onChange={e => {
-								this.setState(
-									processDeck(this.state.pool, parseDeck(e.target.value)),
-								);
-							}}
-							ref={this.deckRef}
-							onClick={e => {
-								e.target.setSelectionRange(0, 999);
-							}}
-						/>
-					</label>
-					<span
-						className={'ico e' + this.state.mark}
-						style={{
-							position: 'absolute',
-							left: '66px',
-							top: '200px',
-						}}
-					/>
-				</>
-			);
-		}
-	},
-);
+	return (
+		<>
+			<Editor
+				cards={Cards}
+				deck={deck}
+				mark={mark}
+				pool={pool}
+				cardMinus={cardMinus}
+				setDeck={deck => {
+					store.store.dispatch(
+						store.updateOrig({
+							deck: etgutil.encodedeck(deck) + etgutil.toTrueMarkSuffix(mark),
+						}),
+					);
+				}}
+				setMark={mark => {
+					store.store.dispatch(
+						store.updateOrig({
+							deck: deckCode.slice(0, -5) + etgutil.toTrueMarkSuffix(mark),
+						}),
+					);
+				}}
+			/>
+			<input
+				type="button"
+				value="Exit"
+				onClick={() => {
+					const update = { deck: deckCode };
+					userEmit('updateorig', update);
+					store.store.dispatch(store.updateOrig(update));
+					store.store.dispatch(store.doNav(import('./MainMenu.jsx')));
+				}}
+				style={{
+					position: 'absolute',
+					left: '8px',
+					top: '110px',
+				}}
+			/>
+			<label
+				style={{
+					position: 'absolute',
+					left: '536px',
+					top: '238px',
+				}}>
+				Deck&nbsp;
+				<input
+					autoFocus
+					value={deckCode}
+					onChange={e => {
+						store.store.dispatch(
+							store.updateOrig({ deck: parseDeck(e.target.value) }),
+						);
+					}}
+					ref={deckRef}
+					onClick={e => {
+						e.target.setSelectionRange(0, 999);
+					}}
+				/>
+			</label>
+			<span
+				className={'ico e' + mark}
+				style={{
+					position: 'absolute',
+					left: '66px',
+					top: '200px',
+				}}
+			/>
+		</>
+	);
+}
