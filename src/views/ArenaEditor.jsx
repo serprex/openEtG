7905834381d97b70d41d82e1450bdb58403ff5d2
Demur Rumed <junkmute@hotmail.com>
2023-06-09 04:00:07 +0000

    Port ArenaEditor & Library to hooks

--- src/views/ArenaEditor.jsx
+++ src/views/ArenaEditor.jsx
@@ -1,5 +1,5 @@
-import { Component } from 'react';
-import { connect } from 'react-redux';
+import { Fragment, useMemo, useState } from 'react';
+import { useSelector } from 'react-redux';
 
 import Cards from '../Cards.js';
 import * as etgutil from '../etgutil.js';
@@ -8,232 +8,195 @@ import * as store from '../store.jsx';
 import { chain } from '../util.js';
 import Editor from '../Components/Editor.jsx';
 
+const arpts = 400;
 const artable = {
-	hp: { min: 80, incr: 20, cost: 1 },
+	hp: { min: 60, incr: 20, cost: 1 },
 	mark: { cost: 20 },
 	draw: { cost: 100 },
 };
-function attrval(x, d) {
-	x = +x;
-	return x === 0 ? 0 : x || d;
+function AttrUi({ attr, sumscore, arpts, setAttr }) {
+	return ['hp', 'mark', 'draw'].map((name, y) => {
+		const top = `${128 + y * 20}px`;
+		const { min = 0, incr = 1 } = artable[name];
+		const value = attr[name];
+		return (
+			<Fragment key={y}>
+				<div
+					style={{
+						position: 'absolute',
+						left: '4px',
+						top,
+					}}>
+					{name}
+				</div>
+				{value - incr >= min && (
+					<input
+						type="button"
+						value="-"
+						onClick={() => setAttr({ ...attr, [name]: value - incr })}
+						style={{
+							position: 'absolute',
+							left: '38px',
+							top,
+							width: '14px',
+						}}
+					/>
+				)}
+				{sumscore + incr * artable[name].cost <= arpts && (
+					<input
+						type="button"
+						value="+"
+						onClick={() => setAttr({ ...attr, [name]: value + incr })}
+						style={{
+							position: 'absolute',
+							left: '82px',
+							top,
+							width: '14px',
+						}}
+					/>
+				)}
+				<div
+					style={{
+						position: 'absolute',
+						left: '56px',
+						top,
+					}}>
+					{value}
+				</div>
+			</Fragment>
+		);
+	});
 }
-function AttrUi({ y, name, value, sumscore, arpts, onChange }) {
-	const top = `${128 + y * 20}px`;
-	const data = artable[name];
-	const incr = data.incr ?? 1;
+
+export default function ArenaEditor(props) {
+	const user = useSelector(({ user }) => user);
+	const pool = useMemo(() => {
+		const baseacard = props.acard.asUpped(false).asShiny(false),
+			pool = [];
+		for (const [code, count] of chain(
+			etgutil.iterraw(user.pool),
+			etgutil.iterraw(user.accountbound),
+		)) {
+			if (
+				Cards.Codes[code] &&
+				(!props.acard ||
+					(!Cards.Codes[code].isOf(baseacard) &&
+						(props.acard.upped || !Cards.Codes[code].upped)))
+			) {
+				pool[code] = (pool[code] ?? 0) + count;
+			}
+		}
+		pool[props.acard.code] = 5;
+		return pool;
+	}, [user.pool, user.accountbound, props.acard]);
+	const [amark, adeck] = useMemo(() => {
+		let mark = 0,
+			adeck = etgutil.decodedeck(props.adeck);
+		for (let i = adeck.length - 1; i >= 0; i--) {
+			if (!Cards.Codes[adeck[i]]) {
+				const index = etgutil.fromTrueMark(adeck[i]);
+				if (~index) {
+					mark = index;
+				}
+				adeck.splice(i, 1);
+			}
+		}
+		return [mark, adeck];
+	}, [props.adeck]);
+	const [deck, setDeck] = useState(adeck);
+	const [mark, setMark] = useState(amark);
+	const cardMinus = useMemo(() => {
+		const cardMinus = Cards.filterDeck(deck, pool);
+		cardMinus[props.acard.code] = 5;
+		return cardMinus;
+	}, [deck, props.acard, pool]);
+
+	const [attr, setAttr] = useState(() => ({
+		hp: props.ainfo.hp ?? 160,
+		mark: props.ainfo.mark ?? 2,
+		draw: props.ainfo.draw ?? 2,
+	}));
+
+	let sumscore = 0;
+	for (const k in artable) {
+		sumscore += attr[k] * artable[k].cost;
+	}
+	const acode = props.acard.code;
 	return (
 		<>
+			<Editor
+				cards={Cards}
+				deck={[acode, acode, acode, acode, acode].concat(deck)}
+				mark={mark}
+				pool={pool}
+				cardMinus={cardMinus}
+				setDeck={deck => {
+					setDeck(deck.filter(x => x !== acode).sort(Cards.codeCmp));
+				}}
+				setMark={setMark}
+				noupped={!props.acard.upped}
+			/>
+			<AttrUi attr={attr} sumscore={sumscore} arpts={arpts} setAttr={setAttr} />
 			<div
 				style={{
 					position: 'absolute',
 					left: '4px',
-					top,
+					top: '188px',
 				}}>
-				{name}
+				{(arpts - sumscore) / 20}
 			</div>
-			{value - incr >= (data.min ?? 0) && (
-				<input
-					type="button"
-					value="-"
-					onClick={() => onChange(value - incr)}
-					style={{
-						position: 'absolute',
-						left: '38px',
-						top,
-						width: '14px',
-					}}
-				/>
-			)}
-			{sumscore + incr * artable[name].cost <= arpts && (
-				<input
-					type="button"
-					value="+"
-					onClick={() => onChange(value + incr)}
-					style={{
-						position: 'absolute',
-						left: '82px',
-						top,
-						width: '14px',
-					}}
-				/>
-			)}
-			<div
+			<input
+				type="button"
+				value="Save & Exit"
+				onClick={() => {
+					if (!Cards.isDeckLegal(deck, user) || sumscore > arpts) {
+						store.store.dispatch(
+							store.chatMsg(
+								'Invalid deck, 35 cards required before submission',
+								'System',
+							),
+						);
+						return;
+					}
+					const data = {
+						d: etgutil.encodedeck(deck) + etgutil.toTrueMarkSuffix(mark),
+						lv: +props.acard.upped,
+						hp: attr.hp,
+						mark: attr.mark,
+						draw: attr.draw,
+					};
+					if (!props.acreate) {
+						data.mod = true;
+					}
+					sock.userEmit('setarena', data);
+					if (props.acreate && props.ainfo.day > 0) {
+						store.store.dispatch(
+							store.updateUser({
+								gold: user.gold + Math.min(props.ainfo.day * 25, 350),
+							}),
+						);
+					}
+					store.store.dispatch(store.chatMsg('Arena deck submitted', 'System'));
+					store.store.dispatch(store.doNav(import('../views/MainMenu.jsx')));
+				}}
 				style={{
 					position: 'absolute',
-					left: '56px',
-					top,
-				}}>
-				{value}
-			</div>
+					left: '8px',
+					top: '58px',
+				}}
+			/>
+			<input
+				type="button"
+				value="Exit"
+				onClick={() => {
+					store.store.dispatch(store.doNav(import('../views/ArenaInfo.jsx')));
+				}}
+				style={{
+					position: 'absolute',
+					left: '8px',
+					top: '84px',
+				}}
+			/>
 		</>
 	);
 }
-
-const arpts = 400;
-export default connect(({ user }) => ({ user }))(
-	class ArenaEditor extends Component {
-		constructor(props) {
-			super(props);
-			const baseacard = props.acard.asUpped(false).asShiny(false);
-			const pool = [];
-			for (const [code, count] of chain(
-				etgutil.iterraw(props.user.pool),
-				etgutil.iterraw(props.user.accountbound),
-			)) {
-				if (
-					Cards.Codes[code] &&
-					(!props.acard ||
-						(!Cards.Codes[code].isOf(baseacard) &&
-							(props.acard.upped || !Cards.Codes[code].upped)))
-				) {
-					pool[code] = (pool[code] ?? 0) + count;
-				}
-			}
-			pool[this.props.acard.code] = 5;
-			let mark = 0,
-				deck = etgutil.decodedeck(props.adeck);
-			for (let i = deck.length - 1; i >= 0; i--) {
-				if (!Cards.Codes[deck[i]]) {
-					const index = etgutil.fromTrueMark(deck[i]);
-					if (~index) {
-						mark = index;
-					}
-					deck.splice(i, 1);
-				}
-			}
-			this.state = {
-				hp: attrval(props.ainfo.hp, 160),
-				mark: attrval(props.ainfo.mark, 2),
-				draw: attrval(props.ainfo.draw, 2),
-				pool,
-				deck,
-				dmark: mark,
-			};
-		}
-
-		setDeck = deck => {
-			deck.sort(Cards.codeCmp);
-			const cardMinus = Cards.filterDeck(deck, this.state.pool);
-			const acode = this.props.acard.code;
-			cardMinus[acode] = 5;
-			this.setState({ cardMinus, deck: deck.filter(x => x !== acode) });
-		};
-
-		render() {
-			let sumscore = 0;
-			for (const k in artable) {
-				sumscore += this.state[k] * artable[k].cost;
-			}
-			const acode = this.props.acard.code;
-			return (
-				<>
-					<Editor
-						cards={Cards}
-						deck={[acode, acode, acode, acode, acode].concat(this.state.deck)}
-						mark={this.state.dmark}
-						pool={this.state.pool}
-						cardMinus={this.state.cardMinus}
-						setDeck={this.setDeck}
-						setMark={dmark => this.setState({ dmark })}
-						noupped={!this.props.acard.upped}
-					/>
-					<AttrUi
-						y={0}
-						name="hp"
-						value={this.state.hp}
-						sumscore={sumscore}
-						arpts={arpts}
-						onChange={val => this.setState({ hp: val })}
-					/>
-					<AttrUi
-						y={1}
-						name="mark"
-						value={this.state.mark}
-						sumscore={sumscore}
-						arpts={arpts}
-						onChange={val => this.setState({ mark: val })}
-					/>
-					<AttrUi
-						y={2}
-						name="draw"
-						value={this.state.draw}
-						sumscore={sumscore}
-						arpts={arpts}
-						onChange={val => this.setState({ draw: val })}
-					/>
-					<div
-						style={{
-							position: 'absolute',
-							left: '4px',
-							top: '188px',
-						}}>
-						{(arpts - sumscore) / 20}
-					</div>
-					<input
-						type="button"
-						value="Save & Exit"
-						onClick={() => {
-							if (
-								!Cards.isDeckLegal(this.state.deck, this.props.user) ||
-								sumscore > arpts
-							) {
-								this.props.dispatch(
-									store.chatMsg(
-										'Invalid deck, 35 cards required before submission',
-										'System',
-									),
-								);
-								return;
-							}
-							const data = {
-								d:
-									etgutil.encodedeck(this.state.deck) +
-									etgutil.toTrueMarkSuffix(this.state.dmark),
-								lv: +this.props.acard.upped,
-								hp: this.state.hp,
-								mark: this.state.mark,
-								draw: this.state.draw,
-							};
-							if (!this.props.acreate) {
-								data.mod = true;
-							}
-							sock.userEmit('setarena', data);
-							if (this.props.acreate && this.props.ainfo.day > 0) {
-								this.props.dispatch(
-									store.updateUser({
-										gold:
-											this.props.user.gold +
-											Math.min(this.props.ainfo.day * 25, 350),
-									}),
-								);
-							}
-							this.props.dispatch(
-								store.chatMsg('Arena deck submitted', 'System'),
-							);
-							this.props.dispatch(store.doNav(import('../views/MainMenu.jsx')));
-						}}
-						style={{
-							position: 'absolute',
-							left: '8px',
-							top: '58px',
-						}}
-					/>
-					<input
-						type="button"
-						value="Exit"
-						onClick={() => {
-							this.props.dispatch(
-								store.doNav(import('../views/ArenaInfo.jsx')),
-							);
-						}}
-						style={{
-							position: 'absolute',
-							left: '8px',
-							top: '84px',
-						}}
-					/>
-				</>
-			);
-		}
-	},
-);
--- src/views/Library.jsx
+++ src/views/Library.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 
 import Cards from '../Cards.js';
 import * as sock from '../sock.jsx';
@@ -7,29 +7,32 @@ import * as etgutil from '../etgutil.js';
 import * as userutil from '../userutil.js';
 import * as Components from '../Components/index.jsx';
 
-export default class Library extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			card: null,
-			showbound: false,
-			pool: '',
-			bound: '',
-		};
-	}
+export default function Library(props) {
+	const [data, setData] = useState({});
+	const [card, setCard] = useState(null);
+	const [showBound, setShowBound] = useState(false);
 
-	componentDidMount() {
-		store.store.dispatch(
-			store.setCmds({
-				librarygive: data => this.setState(data),
-			}),
-		);
-		sock.emit({ x: 'librarywant', f: this.props.name });
-	}
+	useEffect(() => {
+		store.store.dispatch(store.setCmds({ librarygive: setData }));
+		sock.emit({ x: 'librarywant', f: props.name });
+	}, [props.name]);
 
-	render() {
-		const cardpool = etgutil.deck2pool(this.state.pool),
-			boundpool = etgutil.deck2pool(this.state.bound),
+	const {
+		progressmax,
+		progress,
+		shinyprogress,
+		reprog,
+		reprogmax,
+		cardpool,
+		boundpool,
+	} = useMemo(() => {
+		let progressmax = 0,
+			progress = 0,
+			shinyprogress = 0,
+			reprog = [],
+			reprogmax = [];
+		const cardpool = etgutil.deck2pool(data.pool),
+			boundpool = etgutil.deck2pool(data.bound),
 			codeprog = code => {
 				const upcode = etgutil.asUpped(code, true);
 				return Math.min(
@@ -39,11 +42,6 @@ export default class Library extends Component {
 					42,
 				);
 			};
-		let progressmax = 0,
-			progress = 0,
-			shinyprogress = 0,
-			reprog = [],
-			reprogmax = [];
 		Cards.Codes.forEach((card, code) => {
 			if (!card.upped && !card.shiny && card.type && !card.getStatus('token')) {
 				progressmax += 42;
@@ -55,114 +53,123 @@ export default class Library extends Component {
 				shinyprogress += codeprog(etgutil.asShiny(code, true));
 			}
 		});
-		const children = [];
+		return {
+			progressmax,
+			progress,
+			shinyprogress,
+			reprog,
+			reprogmax,
+			cardpool,
+			boundpool,
+		};
+	}, [data.pool, data.bound]);
+	const children = [];
+	for (let e = 0; e < 13; e++) {
+		children.push(
+			<span
+				className={`ico e${e}`}
+				style={{
+					position: 'absolute',
+					left: `${36 + e * 53}px`,
+					top: '54px',
+				}}
+			/>,
+		);
+	}
+	for (let r = 1; r < 4; r++) {
+		children.push(
+			<span
+				className={`ico r${r}`}
+				style={{
+					position: 'absolute',
+					left: '8px',
+					top: `${64 + r * 32}px`,
+				}}
+			/>,
+		);
 		for (let e = 0; e < 13; e++) {
+			const idx = r * 13 + e;
 			children.push(
 				<span
-					className={`ico e${e}`}
 					style={{
 						position: 'absolute',
 						left: `${36 + e * 53}px`,
-						top: '54px',
-					}}
-				/>,
-			);
-		}
-		for (let r = 1; r < 4; r++) {
-			children.push(
-				<span
-					className={`ico r${r}`}
-					style={{
-						position: 'absolute',
-						left: '8px',
 						top: `${64 + r * 32}px`,
-					}}
-				/>,
+						fontSize: '12px',
+						textShadow:
+							reprog[idx] === reprogmax[idx] ? '1px 1px 2px #fff' : undefined,
+					}}>
+					{reprog[idx] ?? 0} / {reprogmax[idx] ?? 0}
+				</span>,
 			);
-			for (let e = 0; e < 13; e++) {
-				const idx = r * 13 + e;
-				children.push(
-					<span
-						style={{
-							position: 'absolute',
-							left: `${36 + e * 53}px`,
-							top: `${64 + r * 32}px`,
-							fontSize: '12px',
-							textShadow:
-								reprog[idx] === reprogmax[idx] ? '1px 1px 2px #fff' : undefined,
-						}}>
-						{reprog[idx] ?? 0} / {reprogmax[idx] ?? 0}
-					</span>,
-				);
-			}
 		}
-		return (
-			<>
-				<span
-					style={{
-						position: 'absolute',
-						left: '100px',
-						top: '8px',
-						whiteSpace: 'pre',
-					}}>
-					{`Wealth ${
-						this.state.gold + Math.round(userutil.calcWealth(Cards, cardpool))
-					}\nGold ${this.state.gold}`}
-				</span>
-				<span
-					style={{
-						position: 'absolute',
-						left: '320px',
-						top: '8px',
-						whiteSpace: 'pre',
-					}}>
-					ZE Progress {progress} / {progressmax}
-					{'\nSZE Progress '}
-					{shinyprogress} / {progressmax}
-				</span>
-				<span
-					style={{
-						position: 'absolute',
-						left: '540px',
-						top: '8px',
-						whiteSpace: 'pre',
-					}}>
-					{`PvE ${this.state.aiwins} - ${this.state.ailosses}\nPvP ${this.state.pvpwins} - ${this.state.pvplosses}`}
-				</span>
-				<Components.Card x={734} y={8} card={this.state.card} />
-				<input
-					type="button"
-					value="Toggle Bound"
-					style={{
-						position: 'absolute',
-						left: '5px',
-						top: '554px',
-					}}
-					onClick={() => {
-						this.setState({ showbound: !this.state.showbound });
-					}}
-				/>
-				<Components.ExitBtn x={5} y={8} />
-				<input
-					type="button"
-					value="Export"
-					style={{
-						position: 'absolute',
-						left: '5px',
-						top: '28px',
-					}}
-					onClick={() => open('/collection/' + this.props.name, '_blank')}
-				/>
-				<Components.CardSelector
-					cards={Cards}
-					cardpool={this.state.showbound ? boundpool : cardpool}
-					filterboth
-					onMouseOver={card => {
-						card !== this.state.card && this.setState({ card });
-					}}
-				/>
-				{children}
-			</>
-		);
 	}
+	return (
+		<>
+			<span
+				style={{
+					position: 'absolute',
+					left: '100px',
+					top: '8px',
+					whiteSpace: 'pre',
+				}}>
+				{`Wealth ${
+					data.gold + Math.round(userutil.calcWealth(Cards, cardpool))
+				}\nGold ${data.gold}`}
+			</span>
+			<span
+				style={{
+					position: 'absolute',
+					left: '320px',
+					top: '8px',
+					whiteSpace: 'pre',
+				}}>
+				ZE Progress {progress} / {progressmax}
+				{'\nSZE Progress '}
+				{shinyprogress} / {progressmax}
+			</span>
+			<span
+				style={{
+					position: 'absolute',
+					left: '540px',
+					top: '8px',
+					whiteSpace: 'pre',
+				}}>
+				{`PvE ${data.aiwins} - ${data.ailosses}\nPvP ${data.pvpwins} - ${data.pvplosses}`}
+			</span>
+			<Components.Card x={734} y={8} card={card} />
+			<input
+				type="button"
+				value="Toggle Bound"
+				style={{
+					position: 'absolute',
+					left: '5px',
+					top: '554px',
+				}}
+				onClick={() => {
+					setShowBound(!showBound);
+				}}
+			/>
+			<Components.ExitBtn x={5} y={8} />
+			<input
+				type="button"
+				value="Export"
+				style={{
+					position: 'absolute',
+					left: '5px',
+					top: '28px',
+				}}
+				onClick={() => open('/collection/' + props.name, '_blank')}
+			/>
+			<Components.CardSelector
+				cards={Cards}
+				cardpool={showBound ? boundpool : cardpool}
+				filterboth
+				onMouseOver={c => {
+					c !== card && setCard(c);
+				}}
+			/>
+			{children}
+		</>
+	);
 }
