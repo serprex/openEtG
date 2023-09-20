34293cb555ef9f44dea02a16587ff6f9ce903d2b
Demur Rumed <junkmute@hotmail.com>
2023-09-20 17:52:53 +0000

    adjust colors, particularly saturate red chat text

--- src/deckgen.js
+++ src/deckgen.js
@@ -1,22 +1,23 @@
-import * as etg from './etg.js';
 import * as wasm from './rs/pkg/etg.js';
 import { randint, upto } from './util.js';
 import { encodedeck } from './etgutil.js';
 
-export const aiNames = {
-	[etg.Air]: ['Ari', 'es'],
-	[etg.Aether]: ['Aeth', 'eric'],
-	[etg.Darkness]: ['Shad', 'ow'],
-	[etg.Death]: ['Mor', 'tis'],
-	[etg.Earth]: ['Ter', 'ra'],
-	[etg.Entropy]: ['Dis', 'cord'],
-	[etg.Fire]: ['Pyr', 'ofuze'],
-	[etg.Gravity]: ['Mas', 'sa'],
-	[etg.Life]: ['Vit', 'al'],
-	[etg.Light]: ['Lum', 'iel'],
-	[etg.Time]: ['Chr', 'onos'],
-	[etg.Water]: ['Aqua', 'rius'],
-};
+// prettier-ignore
+export const aiNames = [
+	,,
+	'Dis', 'cord',
+	'Mor', 'tis',
+	'Mas', 'sa',
+	'Ter', 'ra',
+	'Vit', 'al',
+	'Pyr', 'ofuze',
+	'Aqua', 'rius',
+	'Lum', 'iel',
+	'Ari', 'es',
+	'Chr', 'onos',
+	'Shad', 'ow',
+	'Aeth', 'eric',
+];
 
 export function deckgen(uprate, markpower, maxRarity) {
 	const es = upto(150);
@@ -24,7 +25,7 @@ export function deckgen(uprate, markpower, maxRarity) {
 		const e1 = ((es / 12) | 0) + 1,
 			e2 = (es % 12) + 1;
 		return [
-			aiNames[e1][0] + aiNames[e2][1],
+			aiNames[e1 * 2] + aiNames[e2 * 2 + 1],
 			encodedeck(
 				wasm.deckgen_duo(e1, e2, uprate, markpower, maxRarity, randint()),
 			),
@@ -42,7 +43,7 @@ export function deckgenAi4() {
 		e1 = ((es / 12) | 0) + 1,
 		e2 = (es % 12) + 1;
 	return [
-		aiNames[e1][0] + aiNames[e2][1],
+		aiNames[e1 * 2] + aiNames[e2 * 2 + 1],
 		encodedeck(wasm.deckgen_ai4(e1, e2)),
 	];
 }
